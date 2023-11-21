02979f0b5cfa29fdb3006bd57a7d1ba3922a2dbe
Rungsikorn Rungsikavarnich <rungsikorn.rungsikavarnich@woven-planet.global>
2023-11-21 08:59:18 +0000

    add css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "monday-ui-react-core/dist/main.css"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
