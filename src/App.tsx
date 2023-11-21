40f9a65792ce9aa31e851e6f1bc7a6093614ba9b
Rungsikorn Rungsikavarnich <rungsikorn.rungsikavarnich@woven-planet.global>
2023-11-21 07:01:42 +0000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { AttentionBox } from 'monday-ui-react-core';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AttentionBox 
+          title='Hello Apps'
+          text="Notice me!"
+      /> 
     </div>
   );
 }
