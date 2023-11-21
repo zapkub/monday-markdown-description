19cb995ebe0ef2e0759a489f0cdcb51b7267dd9c
Rungsikorn Rungsikavarnich <rungsikorn.rungsikavarnich@woven-planet.global>
2023-11-21 13:49:18 +0000

    add storage with item id

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,66 @@
 import './App.css';
-import { AttentionBox } from 'monday-ui-react-core';
-import { useCallback, useEffect, useState } from 'react';
+import { AttentionBox, Button } from 'monday-ui-react-core';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import Markdown from 'react-markdown'
 import { useMondayClient } from './hooks/MondayProvider';
-
-const markdown = `Here is some JavaScript code:
-
-~~~js
-console.log('It works!')
-~~~
-`
 
 function App() {
 
   const monday = useMondayClient();
 
   useEffect(() => {
-    monday.m?.storage.instance.getItem('mfmde_value').then((res) => {
-      console.log('res:', res)
+    console.log('load item ids')
+    monday.m?.get('itemIds').then((res) => {
+      console.log('item ids', res)
     })
   }, [])
 
   const [text, setText] = useState("")
+  const storageKey = useMemo(() => `mfmd_value_${monday.mContext?.data.itemId}`, [monday])
+
+  const reloadData = useCallback(() => {
+    console.log('load from storage key', storageKey)
+    monday.m?.storage.instance.getItem(storageKey).then(resp => {
+      console.log('resp load data', resp)
+      setText(resp.data.value || '');
+    }) 
+  }, [monday])
+
   const onChange = useCallback((evt) => {
       setText(evt.target.value)
   }, [setText])
 
+
+  useEffect(() => {
+    console.log('context changed, reload data')
+  }, [monday.mContext])
+
   const onSave = useCallback(() => {
-    monday.m?.storage.instance.setItem('mfmd_value', text)
-  }, [text])
-  const onLoad = useCallback(() => {
-    monday.m?.storage.instance.getItem('mfmd_value').then((res) => {
-      console.log('res:', res)
-      setText(res.data.value)
+    console.log('save to storage key', storageKey)
+    monday.m?.storage.instance.setItem(storageKey, text).then((resp) => {
+      console.log('saved', resp)
     })
   }, [text])
+  const onLoad = useCallback(() => {
+    monday.reloadContext();
+  }, [text])
+
+  if (!monday.ready) {
+    return <div>loading</div>
+  }
+
   return (
     <div className="App">
-      <AttentionBox
-        title='Hello Apps'
-        text="Notice me!"
-      />
-      <textarea onChange={onChange} />
-      <Markdown children={text} />
-      <button onClick={onSave}>save</button>
-      <button onClick={onLoad}>load</button>
+      <div>{monday.mContext?.data.itemId}</div>
+      <div className="content-container">
+        <textarea className="md-source" value={text} onChange={onChange} />
+        <Markdown children={text} />
+      </div>
+      <div className="control-container">
+        <Button onClick={onSave}>save</Button>
+        <button onClick={reloadData}>load</button>
+        <button onClick={onLoad}>load context</button>
+      </div>
     </div>
   );
 }
--- src/hooks/MondayProvider.tsx
+++ src/hooks/MondayProvider.tsx
@@ -1,23 +1,42 @@
-import React, { useMemo } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import Monday from 'monday-sdk-js';
 import { MondayClientSdk } from 'monday-sdk-js/types/client-sdk.interface';
 
-
-type MondayProviderContextValue = { m: MondayClientSdk | null; ready: boolean }
-const MondayProviderContext = React.createContext<MondayProviderContextValue>({ m: null, ready: false })
+type MondayCurrentStateContext = {
+    data: {
+        itemId: string
+    }
+}
+type MondayProviderContextValue = { m: MondayClientSdk | null; ready: boolean; mContext: MondayCurrentStateContext | null; reloadContext: () => void }
+const MondayProviderContext = React.createContext<MondayProviderContextValue>({ m: null, ready: false, mContext: null, reloadContext: () => {} })
 
 export const MondayProvider = ({ children }: { children: React.ReactNode }) => {
     const [m, setM] = React.useState<MondayClientSdk | null>(null)
-    React.useEffect(() => {
+    const [mContext, setmContext] = React.useState<MondayCurrentStateContext | null>(null)
+    const [isReady, setIsReady] = React.useState<boolean>(false);
+
+    const reloadContext = useCallback(() => {
+        setIsReady(false);
         const m = Monday();
-        setM(m)
+        m.get('context').then((resp) => {
+            setM(m)
+            setmContext(resp)
+            setIsReady(true)
+        })
     }, [])
+
+    React.useEffect(() => {
+        reloadContext()
+    }, [reloadContext])
 
     const contextValue = useMemo<MondayProviderContextValue>(() => ({
         m,
-        ready: m !== null,
-    }), [m])
+        mContext: mContext,
+        ready: isReady,
+        reloadContext,
+    }), [m, mContext, isReady])
 
+    console.log(contextValue)
 
     return (
         <MondayProviderContext.Provider value={contextValue}>
@@ -34,4 +53,3 @@ export function useMondayClient() {
 
     return contextValue;
 }
-
